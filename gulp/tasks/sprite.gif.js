b758c721be57002d75df683b578baef3841a427e
Maksim Palii <maksim.palii@gmail.com>
2017-02-11 00:00:13 +0200

    delete comment

--- gulp/tasks/sprite.gif.js
+++ gulp/tasks/sprite.gif.js
@@ -3,20 +3,12 @@
 module.exports = function() {
 
     $.gulp.task('sprite:gif', function () {
-        //var spriteData = $.gulp.src('./source/sprite/*.gif')
         return $.gulp.src('./source/sprite/*.gif')
             .pipe($.spritesmith({
                 imgName: 'sprite.gif',
                 cssName: 'sprite-gif.css'
             }))
-            // return spriteData.pipe($.gulp.dest('build/assets/img/sprite/'));
-            //.pipe($.gulp.dest('build/assets/img/output/'));
-
-            .pipe($.gulp.dest($.config.root + '/assets/img/sprite'));
-
-            //.pipe($.gulpif('*.gif', $.gulp.dest($.config.root + '/assets/img/sprite')))
-            //.pipe($.gulpif('*.css', $.gulp.dest($.config.root + '/assets/css/')));
-
+             .pipe($.gulp.dest($.config.root + '/assets/img/sprite'));
     });
 
 };
--- gulp/tasks/sprite.png.js
+++ gulp/tasks/sprite.png.js
@@ -3,19 +3,12 @@
 module.exports = function() {
 
     $.gulp.task('sprite:png', function () {
-        //var spriteData = $.gulp.src('./source/sprite/*.png')
         return $.gulp.src('./source/sprite/*.png')
             .pipe($.spritesmith({
             imgName: 'sprite.png',
             cssName: 'sprite-png.css'
         }))
-       // return spriteData.pipe($.gulp.dest('build/assets/img/sprite/'));
-       //.pipe($.gulp.dest('build/assets/img/output/'));
        .pipe($.gulp.dest($.config.root + '/assets/img/sprite'));
-
-        //.pipe($.gulpif('*.png', $.gulp.dest($.config.root + '/assets/img/sprite')))
-        //.pipe($.gulpif('*.css', $.gulp.dest($.config.root + '/assets/css/')));
-
     });
 
 
